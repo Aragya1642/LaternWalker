537e235aacef4f0a838c69499bbfb5ffa25a81ad
Aragya Goyal <56979762+Aragya1642@users.noreply.github.com>
2024-12-05 18:31:17 -0500

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,14 @@
+#define hipLOffset 95
+#define kneeLOffset 85
+// #define kneeLOffset 180
+#define ankleLOffset 90
+#define hipROffset 90
+// #define kneeROffset 80
+#define kneeROffset 180
+#define ankleROffset 90
+
+#define l1 5
+#define l2 5.7
+
+#define stepClearance 1
+#define stepHeight 10
